f00495e51ea87428b40deba79086aab17999c1aa
UmangThakur15 <umangthakuremail@gmail.com>
2023-06-23 21:12:47 -0400

    Profile Screen 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter} from "react-router-dom";
+import {Routes, Route} from "react-router";
+import MyApp from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        {/*<div className="container">*/}
+          <Routes>
+            <Route index path="/*" element={<MyApp/>}/>
+          </Routes>
+        {/*</div>*/}
+      </BrowserRouter>
   );
 }
 
