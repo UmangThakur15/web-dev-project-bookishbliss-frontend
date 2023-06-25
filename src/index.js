f9d224a07998641b24fbc4d81b35426c66fce078
UmangThakur15 <umangthakuremail@gmail.com>
2023-06-25 15:54:26 -0400

    index

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- 
   <React.StrictMode>
     <App />
   </React.StrictMode>
